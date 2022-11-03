b76b0af9611a731183f1a0a1e60b46fe56750f1e
iamsuvhro <suvhradip@gmail.com>
2022-11-04 01:10:22 +0530

    Adding user account api service

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('id','name','email','username','date_joined','last_login','is_admin','is_staff','github_token')
+    search_fields =('email','username')
+    readonly_fields = ('id','date_joined','last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+
+admin.site.register(Account, AccountAdmin)
--- api/users/views.py
+++ api/users/views.py
@@ -1,6 +1,36 @@
 from http.client import HTTPResponse
-from django.shortcuts import render,HttpResponse
+from urllib import response
+from django.shortcuts import render, HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from users.Services import UserServices
 
-# Create your views here.
-def Home(request):
-    return HttpResponse("Hello")
+
+class UserServicesAPI(APIView):
+    """
+    Method for user services api  
+    """
+    pass
+
+
+@api_view(["POST"])
+def create_user(request):
+    payload = request.data
+    response = UserServices.create_user(payload)
+    return Response(response)
+
+
+@api_view(['POST'])
+def get_user(request):
+    payload = request.data
+    response = UserServices.get_user(payload)
+    return Response(response)
+
+
+@api_view(['POST'])
+def update_user(request):
+    payload = request.data
+    response = UserServices.update_user(payload)
+    return Response(response)
