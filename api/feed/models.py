3439572c4f98e27437d0ca4b11d89c241c955950
iamsuvhro <suvhradip@gmail.com>
2022-12-09 11:39:13 +0530

    adding feed api service

--- api/feed/models.py
+++ api/feed/models.py
@@ -1,3 +1,18 @@
-from django.db import models
 
-# Create your models here.
+from time import time
+from django.db import models
+from django.contrib.auth.models import User
+
+class FeedCard(models.Model):
+    """
+    Model for storing feed card details
+    """
+    card_id = models.IntegerField(primary_key=True)
+    project_title = models.CharField(max_length=100)
+    project_drescriptions = models.TextField(max_length=5000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    repository = models.CharField(max_length=100)
+    card_created = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.project_title
